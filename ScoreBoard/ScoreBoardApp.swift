3b90929ed9547f7ac81fef12a8de165c87526e0a
david-why <david_why@outlook.com>
2025-09-17 12:11:48 +0800

    Initial Commit

---
+++ ScoreBoard/ScoreBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreBoardApp.swift
+//  ScoreBoard
+//
+//  Created by David Wang on 2025/9/17.
+//
+
+import SwiftUI
+
+@main
+struct ScoreBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
